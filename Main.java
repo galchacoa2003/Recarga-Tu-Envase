ef980f7b7ccbd7e78d6686279e0944b843088d31
galchacoa2003 <galchacoa.4347@unimar.edu.ve>
2025-06-08 18:31:02 -0400

    commit add - version 2 (se agrego el menu - aun sin funcionalidad)

--- Main.java
+++ Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
+
+    // Menu principal tiene como objetivo mostrar los datos
     public static void main(String[] args) {
-        System.out.println("Recarga tu envase");
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+        
+        // Bucle principal del menú
+        do {
+            
+            System.out.println("\n|========== Recarga Tu Envase ==========|");
+            System.out.println("|                                       |");
+            System.out.println("| (1). Registrar venta                  |");
+            System.out.println("| (2). Ver ventas registradas           |");
+            System.out.println("| (3). Cancelar última venta            |");
+            System.out.println("|                                       |");
+            System.out.println("| (4). Salir                            |");
+            System.out.println("|======== by Gabriela Alchacoa =========|");
+            System.out.print("|Elige una opción (1-4): ");
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    // Registrar venta
+                    System.out.println("Opción 1: Registrar venta");
+                    break;
+                case 2:
+                    // Ver ventas registradas
+                    System.out.println("Opción 2: Ver ventas registradas");
+                    break;
+                case 3:
+                    // Cancelar última venta
+                    System.out.println("Opción 3: Cancelar última venta");
+                    break;
+                case 4:
+                    // salir
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor elige una opción entre 1 y 4.");
+                    break;
+            }
+
+        } while (opcion != 4); 
+
+        scanner.close();
     }
 }
